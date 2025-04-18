cff34344ee4d5661f7ad99bc97a05e0a9c35935c
Cole Cianflone <colecianflone123@gmail.com>
2025-04-18 14:41:07 -0500

    Got the whole form setup.

--- App.js
+++ App.js
@@ -4,39 +4,50 @@ import { View, Text, TextInput, ScrollView} from 'react-native';
 const App = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [name, setName] = useState('');
   const [dateOfBirth, setDateOfBirth] = useState('');
   const [breed, setBreed] = useState('');
   const [favoriteToy, setFavoriteToy] = useState('');
   return (
-    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#ecf0f1' }}>
-      <ScrollView style={{paddingTop:75}}> 
+    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#ecf0f1', padding:15, paddingTop:30 }}>
+      <ScrollView style={{}}> 
         <Text style={{padding:8, fontSize:20}}>Account Info</Text>
         <InputField
           label="Email"
           value={email}
-          onChangeText={setEmail}
           placeholder="Your email..."    
+          onChangeText={setEmail}
         />
         <InputField
           label="Password"
           value={password}
-          onChangeText={setPassword}
           placeholder="A secure password..."   
           secureTextEntry={true}
+          onChangeText={setPassword}
+        />
+        <InputField
+          label="Confirm Password"
+          value={confirmPassword}
+          placeholder="Confirm your secure password..."   
+          secureTextEntry={true}
+          onChangeText={setConfirmPassword}
+          onSubmitEditing={(e) => {
+            confirmPasswordsMatch(e.nativeEvent.text, password);
+          }}
         />
         <Text style={{paddingTop:20, padding:8, fontSize:20}}>Dog Info</Text>
         <InputField
           label="Name"
           value={name}
-          onChangeText={setName}
           placeholder="Your dogs name..."    
+          onChangeText={setName}
         />
         <InputField
           label="Date of birth"
           value={dateOfBirth}
-          onChangeText={setDateOfBirth}
           placeholder="Your dogs date of birth..."    
+          onChangeText={setDateOfBirth}
         />
         <InputField
           label="Breed"
@@ -55,27 +66,35 @@ const App = () => {
   ); 
 };
 
+function confirmPasswordsMatch(confirmationPassword, originalPassword) {
+  if (confirmationPassword !== originalPassword) {
+    alert('Passwords do not match, please try again.');
+  }
+}
+
 export default App;
 
 export const InputField = ({
   label,
   value,
-  onChangeText,
   placeholder,
   secureTextEntry = false,
+  onChangeText,
+  onSubmitEditing
 }) => (
     <View style={{
       alignContent: 'center',
       justifyContent: 'center',
       padding: 15
     }}>
-      <Text style ={{marginVertical: 10}}>{label}</Text>
+      <Text style ={{fontSize: 15, marginVertical: 10}}>{label}</Text>
       <TextInput
-        style={{padding:8, backgroundColor: '#f5f5f5'}} 
+        style={{fontSize: 15, padding:8, backgroundColor: '#f5f5f5'}} 
         value={value}
-        onChangeText={text => onChangeText(text)}
         placeholder={placeholder}
         secureTextEntry={secureTextEntry}
+        onChangeText={onChangeText}
+        onSubmitEditing={onSubmitEditing}
       />
     </View>
 );
