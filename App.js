fc5afbe4103bb9ef76f6e378ac3565eddc2d0817
Cole Cianflone <colecianflone123@gmail.com>
2025-04-22 19:13:28 -0500

    implemented custom component from other form and formik

--- App.js
+++ App.js
@@ -1,100 +1,95 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, ScrollView} from 'react-native';
+import React from 'react';
+import { View, Text, ScrollView, Button, Alert } from 'react-native';
+import { Formik } from 'formik';
+import InputWithLabel from './components/InputWithLabel';
 
-const App = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [name, setName] = useState('');
-  const [dateOfBirth, setDateOfBirth] = useState('');
-  const [breed, setBreed] = useState('');
-  const [favoriteToy, setFavoriteToy] = useState('');
+export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#ecf0f1', padding:15, paddingTop:30 }}>
-      <ScrollView style={{}}> 
-        <Text style={{padding:8, fontSize:20}}>Account Info</Text>
-        <InputField
-          label="Email"
-          value={email}
-          placeholder="Your email..."    
-          onChangeText={setEmail}
-        />
-        <InputField
-          label="Password"
-          value={password}
-          placeholder="A secure password..."   
-          secureTextEntry={true}
-          onChangeText={setPassword}
-        />
-        <InputField
-          label="Confirm Password"
-          value={confirmPassword}
-          placeholder="Confirm your secure password..."   
-          secureTextEntry={true}
-          onChangeText={setConfirmPassword}
-          onSubmitEditing={(e) => {
-            confirmPasswordsMatch(e.nativeEvent.text, password);
-          }}
-        />
-        <Text style={{paddingTop:20, padding:8, fontSize:20}}>Dog Info</Text>
-        <InputField
-          label="Name"
-          value={name}
-          placeholder="Your dogs name..."    
-          onChangeText={setName}
-        />
-        <InputField
-          label="Date of birth"
-          value={dateOfBirth}
-          placeholder="Your dogs date of birth..."    
-          onChangeText={setDateOfBirth}
-        />
-        <InputField
-          label="Breed"
-          value={breed}
-          onChangeText={setBreed}
-          placeholder="Your dogs breed..."    
-        />
-        <InputField
-          label="Favorite Toy"
-          value={favoriteToy}
-          onChangeText={setFavoriteToy}
-          placeholder="Your dogs favorite toy..."    
-        />
-      </ScrollView>  
-    </View>
-  ); 
-};
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+        confirmPassword: '',
+        name: '',
+        dateOfBirth: '',
+        breed: '',
+        favoriteToy: '',
+      }}
+      onSubmit={values => {
+        console.log('Submitted values:', values);
+        Alert.alert('Form submitted!', JSON.stringify(values, null, 2));
+      }}
+    >
+      {({
+        handleChange,
+        handleSubmit,
+        values,
+      }) => (
+        <View style={{ flex: 1, padding: 15, paddingTop: 30, backgroundColor: '#ecf0f1' }}>
+          <ScrollView>
 
-function confirmPasswordsMatch(confirmationPassword, originalPassword) {
-  if (confirmationPassword !== originalPassword) {
-    alert('Passwords do not match, please try again.');
-  }
+            <Text style={{ padding: 8, fontSize: 20 }}>Account Info</Text>
+
+            <InputWithLabel
+              label="Email"
+              value={values.email}
+              placeholder="Your email…"
+              onChangeText={handleChange('email')}
+            />
+
+            <InputWithLabel
+              label="Password"
+              value={values.password}
+              placeholder="A secure password…"
+              secureTextEntry
+              onChangeText={handleChange('password')}
+            />
+
+            <InputWithLabel
+              label="Confirm Password"
+              value={values.confirmPassword}
+              placeholder="Confirm your password…"
+              secureTextEntry
+              onChangeText={handleChange('confirmPassword')}
+            />
+
+            <Text style={{ paddingTop: 20, padding: 8, fontSize: 20 }}>Dog Info</Text>
+
+            <InputWithLabel
+              label="Name"
+              value={values.name}
+              placeholder="Your dog’s name…"
+              onChangeText={handleChange('name')}
+            />
+
+            <InputWithLabel
+              label="Date of birth"
+              value={values.dateOfBirth}
+              placeholder="YYYY-MM-DD"
+              onChangeText={handleChange('dateOfBirth')}
+            />
+
+            <InputWithLabel
+              label="Breed"
+              value={values.breed}
+              placeholder="Your dog’s breed…"
+              onChangeText={handleChange('breed')}
+            />
+
+            <InputWithLabel
+              label="Favorite Toy"
+              value={values.favoriteToy}
+              placeholder="Your dog’s favorite toy…"
+              onChangeText={handleChange('favoriteToy')}
+            />
+
+            <View style={{ marginTop: 30, marginHorizontal: 15 }}>
+              <Button title="Submit" onPress={handleSubmit} />
+            </View>
+
+          </ScrollView>
+        </View>
+      )}
+    </Formik>
+  );
 }
-
-export default App;
-
-export const InputField = ({
-  label,
-  value,
-  placeholder,
-  secureTextEntry = false,
-  onChangeText,
-  onSubmitEditing
-}) => (
-    <View style={{
-      alignContent: 'center',
-      justifyContent: 'center',
-      padding: 15
-    }}>
-      <Text style ={{fontSize: 15, marginVertical: 10}}>{label}</Text>
-      <TextInput
-        style={{fontSize: 15, padding:8, backgroundColor: '#f5f5f5'}} 
-        value={value}
-        placeholder={placeholder}
-        secureTextEntry={secureTextEntry}
-        onChangeText={onChangeText}
-        onSubmitEditing={onSubmitEditing}
-      />
-    </View>
-);
